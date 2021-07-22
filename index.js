1829da2f44a8d6e74826c4e1c8010793f5ce3fbe
Ajat Darojat <ajatdarojat45@gmail.com>
2021-07-23 06:55:34 +0700

    add login feature

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.post("/", (req, rest) => {
+	const { email, password } = req.body;
+
+	rest.status(200).json({
+		email,
+		token: "initoken",
+	});
+});
+
 app.listen(PORT, () => {
 	`app listening on PORT: ${PORT}`;
 });
